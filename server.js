45c3644d1ddce54e6ffd9de857c55f678fb3e30e
DP_OP <deepakpattnayak2013@gmail.com>
2021-06-09 15:35:13 +0530

    nodemon running fine

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 console.log("fuck this shit")
 
-
 const express = require('express')
 const app = express();
 
-const hbs = require('express-handlebars');
+// const hbs = require('express-handlebars');
 
-const path = require('path');
-const { dirname } = require('path/posix');
+// const path = require('path');
+// const { dirname } = require('path/posix');
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use(express.static(path.join(__dirname,'public')));
+// app.use(express.static(path.join(__dirname,'public')));
 
-app.set('view engine','hbs');
-app.engine('hbs',hbs({
-    extname:'hbs',
-    defaultView:'default',
-    layoutsDir:path.join(__dirname,'views'),
-    partialsDir:path.join(__dirname,'views//partials')
-}))
+// app.set('view engine','hbs');
+// app.engine('hbs',hbs({
+//     extname:'hbs',
+//     defaultView:'default',
+//     layoutsDir:path.join(__dirname,'views'),
+//     partialsDir:path.join(__dirname,'views//partials')
+// }))
+app.listen(3000, () => console.log('Server is started on http://localhost:3000'));
 
 app.get('/',(req,res)=>{
-    res.render('main.hbs');
-})
+    res.send('hello world');
+});
 
-app.listen(3000, () => console.log('Server is started on http://localhost:3000'));
+//nodemon server.js
+
