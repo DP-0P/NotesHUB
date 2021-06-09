50bd22df8803683dfadff2ffbe0264e74ce1bd26
DP_OP <deepakpattnayak2013@gmail.com>
2021-06-09 14:40:19 +0530

    dunno

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+console.log("fuck this shit")
+
+
 const express = require('express')
 const app = express();
+
+const hbs = require('express-handlebars');
 
 const path = require('path');
 const { dirname } = require('path/posix');
@@ -8,8 +13,16 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname,'public')));
 
+app.set('view engine','hbs');
+app.engine('hbs',hbs({
+    extname:'hbs',
+    defaultView:'default',
+    layoutsDir:path.join(__dirname,'views'),
+    partialsDir:path.join(__dirname,'views//partials')
+}))
+
 app.get('/',(req,res)=>{
-    res.send("<h3>yo yo chal </h3>");
+    res.render('main.hbs');
 })
 
 app.listen(3000, () => console.log('Server is started on http://localhost:3000'));
