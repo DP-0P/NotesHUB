8c26279a3ba0592c0e636a5522c454d69ca24abe
DP_OP <deepakpattnayak2013@gmail.com>
2021-06-12 11:00:31 +0530

    aaaaaaaaaaaahhhhhhhhh

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyparser = require('body-parser');
+const path = require('path');
+const { dirname } = require('path');
 
 const app = express();
 
@@ -13,10 +15,17 @@ const PORT = process.env.PORT || 8080;
 
 app.use(morgan('tiny'));
 
-app.use(bodyparser.urlencoded({extended:true}))
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.set('view engine','ejs');
+// app.set('views',path.resolve(--dirname,'views/ejs'));
+
+app.use('/css',express.static(path.resolve(__dirname,'assets/css')));
+app.use('/img',express.static(path.resolve(__dirname,'assets/img')));
+app.use('/js',express.static(path.resolve(__dirname,'assets/js')));
 
 app.get('/',(req,res) => {
-    res.send('crud application')
+    res.render('index');
 });
 
 app.listen(PORT, () => {
