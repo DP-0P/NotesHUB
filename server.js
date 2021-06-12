09a9cca8f26f8afa99bbaf3c7527927e3b8fb4ab
DP_OP <deepakpattnayak2013@gmail.com>
2021-06-12 06:48:27 +0530

    lets monGO

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 console.log("fuck this shit")
 
 const express = require('express');
-const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+
 const app = express();
 
-const MongoClinent = require('mongodb').MongoClient;
+dotenv.config({path:'config.env'});
+
+const PORT = process.env.PORT || 8080;
+
+app.use(morgan('tiny'));
+
+app.use(bodyparser.urlencoded({extended:true}))
+
+app.get('/',(req,res) => {
+    res.send('crud application')
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+});
+
+// const bodyParser = require('body-parser');
+
+// const MongoClinent = require('mongodb').MongoClient;
 
 
 
@@ -28,22 +49,23 @@ const MongoClinent = require('mongodb').MongoClient;
 //     layoutsDir:path.join(__dirname,'views'),
 //     partialsDir:path.join(__dirname,'views//partials')
 // }))
-app.listen(3000, () => console.log('Server is started on http://localhost:3000'));
+// app.listen(3000, () => console.log('Server is started on http://localhost:3000'));
 
-let db;
+// let db;
 
 // app.get('/',(req,res)=>{
 //     res.sendFile(__dirname + '/upload.html');
 // });
 
-const mdbClient = 'mongodb+srv://dp:deepak123456@cluster0.xfvxq.mongodb.net/first?retryWrites=true&w=majority;'
+// const mdbClient = 'mongodb+srv://dp:deepak123456@cluster0.xfvxq.mongodb.net/first?retryWrites=true&w=majority;'
 
-MongoClinent.connect(mdbClient,{
-    useUnifiedTopology:true
-})
-.then(client => {
-    console.log('Connected to database');
-     db = client.db('first'); 
-})
+// MongoClinent.connect(mdbClient,{
+//     useUnifiedTopology:true
+// })
+// .then(client => {
+//     console.log('Connected to database');
+//      db = client.db('first'); 
+// });
+
 //nodemon server.js
 
